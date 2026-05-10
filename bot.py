069a648f14a05c46d6c63e843211e9eaccf1de81
Smile Care <shoxjaxonmirzakamolov24@gmail.com>
2026-05-10 12:03:59 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -33,6 +33,10 @@ class HealthCheckHandler(BaseHTTPRequestHandler):
         self.send_header("Content-type", "text/plain")
         self.end_headers()
         self.wfile.write(b"OK")
+    def do_HEAD(self):
+    self.send_response(200)
+    self.send_header("Content-type", "text/plain")
+    self.end_headers()
 
     def log_message(self, format, *args):
         return # Loglarni to'ldirmaslik uchun
